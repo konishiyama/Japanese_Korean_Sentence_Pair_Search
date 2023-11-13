eef92731320f58e028b5a99147ce7397809fad04
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2023-11-14 00:07:14 +0900

    added_about_page

--- src/app/components/Cover.tsx
+++
@@ -1,9 +0,0 @@
-export default function Cover() {
-	return (
-		<>
-			<div className="items-center justify-center m-auto">
-          <p>日韓の例文75万ペアからキーワードに合致するものを検索</p> 
-			</div>
-		</>
-	);
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Cover from './components/Cover';
 import Search from './components/Search';
 // import Results from './components/Results';
 import Link from 'next/link';
@@ -7,7 +6,6 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-        {/* <Cover /> */}
         <Search />
     </main>
   )
