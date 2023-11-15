0b2ecc8edb33f9e5807c8007c683f36d69554b41
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2023-11-15 21:30:45 +0900

    Google_analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import GoogleAnalytics from './GoogleAnalytics';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -19,6 +20,11 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className='flex flex-col min-h-screen flex-grow font-sans text-base'>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+            <GoogleAnalytics ga_id= 
+            {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+          ) : null}
+
         <Header />
         <div className='w-full mx-auto my-0 p-6 sm:max-w-4xl lg:w-2/3 lg:max-w-4xl'>
           {children}
